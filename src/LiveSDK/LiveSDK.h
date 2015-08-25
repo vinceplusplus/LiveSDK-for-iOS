d7049c03789d644972224a179cefd24ad16bc82e
vc <vinceplusplus@gmail.com>
2015-08-25 23:55:10 +0800

    added framework target removed static library target

---
+++ src/LiveSDK/LiveSDK.h
@@ -0,0 +1,19 @@
+//
+//  LiveSDK.h
+//  LiveSDK
+//
+//  Created by Vincent Cheung on 8/25/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LiveSDK.
+FOUNDATION_EXPORT double LiveSDKVersionNumber;
+
+//! Project version string for LiveSDK.
+FOUNDATION_EXPORT const unsigned char LiveSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiveSDK/PublicHeader.h>
+
+
